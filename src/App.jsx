5cb8bf620b9c15931ea2cd3112932cbe80f6d444
Mohit1x <mohitvyas746@gmail.com>
2024-08-05 13:02:52 +0530

    set up vite with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-green-800 text-3xl'>lets goo vteeee!!!</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
