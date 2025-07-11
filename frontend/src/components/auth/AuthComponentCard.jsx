04dd065014b6ecb3e3cd54835df652a15b2835a7
mohit1x <mohitvyas746@mail.com>
2025-07-11 12:37:43 +0530

    added handle change and handle submit for the data in auth component card

--- frontend/src/components/auth/AuthComponentCard.jsx
+++ frontend/src/components/auth/AuthComponentCard.jsx
@@ -1,11 +1,17 @@
-const AuthComponentCard = ({
-  tag,
-  desc,
-  label,
-  buttonText,
-  toggleButtonText,
-  onToggle,
-}) => {
+import { useState } from "react";
+
+const AuthComponentCard = ({ tag, desc, label, buttonText }) => {
+  const [data, setData] = useState({ name: "", email: "", password: "" });
+
+  const handleOnChange = (field, event) => {
+    const value = event.target.value;
+    setData((prevData) => ({ ...prevData, [field]: value }));
+  };
+
+  const handleSubmit = () => {
+    console.log(data, "data");
+  };
+
   return (
     <div className="auth-form space-y-5">
       <h1 className="text-2xl font-bold">{tag}</h1>
@@ -16,11 +22,18 @@ const AuthComponentCard = ({
             key={idx}
             type="text"
             placeholder={item}
-            className="w-full bg-[#EEEEEE] placeholder:text-[#808080] placeholder:text-sm p-2 rounded-lg capitalize outline-none"
+            className="w-full bg-[#EEEEEE] placeholder:text-[#808080] placeholder:text-sm p-2 rounded-lg placeholder:capitalize outline-none"
+            value={data[item]}
+            onChange={(e) => handleOnChange(item, e)}
           />
         ))}
       </div>
-      <button>{buttonText}</button>
+      <button
+        onClick={handleSubmit}
+        className="w-fit text-white bg-[#5865F2] mx-auto hover:bg-[#39286E] rounded-lg py-2 px-10 font-semibold text-sm uppercase"
+      >
+        {buttonText}
+      </button>
     </div>
   );
 };
