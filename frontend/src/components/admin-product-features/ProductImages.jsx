55508d97cb985a5ab876669a73711dd728d4d43c
Mohit1x <mohitvyas746@gmail.com>
2025-07-23 19:00:50 +0530

    fixed minor issue in product image uploading

--- frontend/src/components/admin-product-features/ProductImages.jsx
+++ frontend/src/components/admin-product-features/ProductImages.jsx
@@ -4,13 +4,13 @@ export const ProductImages = () => {
   const [selectedImages, setSelectedImages] = useState(0);
 
   const allProductImages = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0P0YFBwN_1KV7_pmpCGaJJ06dtUjXviRBHg67O-zEcvLKfK1GKg0piDD5jMZ1l9a4Yug&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4RacjbJkObqLcj70seqkD1wSbFkudSMq5Eg&s",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJR208OmR7V1g9oUzUBJpAmKWKbmutZJNSUV8Ui8mJHNJAHZMkROb7bQ4556md_IJ0hYA&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPLL9H2jZyCjvYTLhnewfSJMhdeVSvUvz2JujnGO5ppPQfqRGKNbzkcfIJzJ3uU6J4CSQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0P0YFBwN_1KV7_pmpCGaJJ06dtUjXviRBHg67O-zEcvLKfK1GKg0piDD5jMZ1l9a4Yug&usqp=CAU",
   ];
   return (
     <div className="bg-gray-200 rounded-xl h-screen">
-      <div className="flex flex-col gap-2 h-full">
+      <div className="flex flex-col gap-2 h-full p-4">
         <h1>Upload Images</h1>
         <div className="flex flex-col gap-2 p-4 h-[60%]">
           <div className="w-full bg-gray-400 rounded-xl h-[80%]">
